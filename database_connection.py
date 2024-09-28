ad7cc3b05710c2fb0431b2916dbbecbf1f946669
Burn1ngShade <92182982+Burn1ngShade@users.noreply.github.com>
2024-09-28 22:27:07 +0100

    wow

--- database_connection.py
+++ database_connection.py
@@ -4,28 +4,28 @@ import constants as c
 # class for all interaction with the database
 class Database_Connection: 
     def __init__(self, path = c.DEFAULT_DATABASE_PATH): #init connection to the database
-        self.con = sqlite3.connect(path)
-        self.cur = self.con.cursor()
+        self._con = sqlite3.connect(path)
+        self._cur = self._con.cursor()
         
     def close(self, commit = False): # do not use class after closing, commit only needed when modifying database
-        if (commit): self.con.commit()
-        self.con.close()
+        if (commit): self._con.commit()
+        self._con.close()
         
     # --- DATABASE INTERACTION ---
  
     def get_record(self, table, column, column_value, all_records=False): # get record(s) from the database
-        self.cur.execute(f"SELECT * FROM {table} WHERE {column} == '{column_value}'")
-        return self.cur.fetchall() if all_records else self.cur.fetchone()
+        self._cur.execute(f"SELECT * FROM {table} WHERE {column} == '{column_value}'")
+        return self._cur.fetchall() if all_records else self._cur.fetchone()
 
     def insert_record(self, table, column_names, column_values): # insert a new record into the database
         formatted_column_names = ', '.join(name for name in column_names)
         formatted_column_values = ', '.join("?" for col in column_values)
-        self.cur.execute(f'''INSERT INTO {table} ({formatted_column_names}) VALUES ({formatted_column_values})''', column_values)
+        self._cur.execute(f'''INSERT INTO {table} ({formatted_column_names}) VALUES ({formatted_column_values})''', column_values)
 
     def update_record(self, table, column_names, column_values, replace_column, replace_value): # update exisiting record from the database  
         formatted_column_names = ', '.join(f"{col} = ?" for col in column_names)
-        self.cur.execute(f'''UPDATE {table} SET {formatted_column_names} WHERE {replace_column} = ?''', list(column_values) + [replace_value])  
+        self._cur.execute(f'''UPDATE {table} SET {formatted_column_names} WHERE {replace_column} = ?''', list(column_values) + [replace_value])  
         
     def delete_record(self, table, column, column_value): # delete a record from the database
-        self.cur.execute(f"DELETE FROM {table} WHERE {column} = '{column_value}'")
+        self._cur.execute(f"DELETE FROM {table} WHERE {column} = '{column_value}'")
         
